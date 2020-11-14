43b57b82c412dca04a3d71ff477ccb413bf6dc76
Liam-Swain <74320380+Liam-Swain@users.noreply.github.com>
2020-11-14 11:53:28 -0800

    Create paddle.java

---
+++ paddle.java
@@ -0,0 +1,75 @@
+
+public class paddle{
+    public int x, y, direct;
+    public  int width = 15;
+    public  int height = 100;
+    public  final int UP = 0;
+    public  final int DOWN = 1;
+    public  final int NOT_MOVE = 2;
+
+
+    public paddle(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.direct = 2;
+    }
+
+    public void update(){
+
+        if(direct == UP){
+            setY(y - 5);
+        }
+
+        if(direct == DOWN){
+            setY( y + 5);
+        }
+    }
+
+    public void getBigPowerUp(){
+        setHeight(getHeight() * 2);
+        setHeight(getHeight() / 2);
+
+    }
+
+    public void setDirect(int direct){
+        this.direct = direct;
+    }
+
+    public int getDirect(){
+        return direct;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public void setHeight(int height){
+        this.height = height;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+
+}
